322f27cb0c58f4510f9c1ede8918d5ce145e3bc3
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-21 13:53:09 +0100

    test: ensures SignUpController returns 400 if no password nor confirmation password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -3,30 +3,60 @@ import { HttpsRequest } from '../protocols/http'
 import { SignUpController } from './signup'
 
 describe('Sign Up Controller', () => {
+  // Test name param
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
         email: 'email@email.com',
         password: 'password',
-        passwordConfirmation: 'password'
+        confirmationPassword: 'password'
       }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
+  // Test email param
   test('Should return 400 if no email is provided', () => {
     const sut = new SignUpController()
     const httpRequest: HttpsRequest = {
       body: {
         name: 'name',
         password: 'password',
-        passwordConfirmation: 'password'
+        confirmationPassword: 'password'
       }
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+  // Test password param
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest: HttpsRequest = {
+      body: {
+        name: 'name',
+        email: 'email@email.com',
+        confirmationPassword: 'password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+  // Test confirm param
+  test('Should return 400 if no confirmation confirmationPassword is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest: HttpsRequest = {
+      body: {
+        name: 'name',
+        email: 'email@email.com',
+        password: 'password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('confirmationPassword'))
   })
 })

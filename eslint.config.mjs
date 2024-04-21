b425a57937c4d074777de32ab49864bc5f3f70f6
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-21 12:49:43 +0100

    fix: fix configurations to run the application with no crash

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
 import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url)
@@ -11,8 +12,14 @@ const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended })
 
 export default [
+  ...compat.extends('standard'),
+  ...tseslint.configs.recommended,
   {
-    files: ['**/*.js', '**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn'
+    },
+    files: ['src/**/*.js', 'src/**/*.ts', 'src/*.ts'],
     languageOptions: {
       globals: globals.browser,
       sourceType: 'commonjs',
@@ -20,6 +27,5 @@ export default [
         project: './tsconfig.json'
       }
     }
-  },
-  ...compat.extends('standard')
+  }
 ]

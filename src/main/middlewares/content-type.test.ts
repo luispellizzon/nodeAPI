39687af3973c61ad768bcb030055d42747937ecf
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-23 22:41:04 +0100

    feat: ensures requests return JSON as default content-type

---
+++ src/main/middlewares/content-type.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest'
+import app from '../config/app'
+describe('Content-Type Middleware', () => {
+  test('Should return default content-type JSON', async () => {
+    app.get('/test_content-type', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .get('/test_content-type')
+      .expect('content-type', /json/)
+  })
+
+  test('Should return xml type when FORCED', async () => {
+    app.get('/test_content-type_xml', (req, res) => {
+      res.type('xml')
+      res.send()
+    })
+    await request(app)
+      .get('/test_content-type_xml')
+      .expect('content-type', /xml/)
+  })
+})

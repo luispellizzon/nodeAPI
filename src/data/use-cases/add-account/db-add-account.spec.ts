a0a55ffdefef6b832371b0f0ea7aa8ab17efac57
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-23 13:16:17 +0100

    test: ensure DbAddAccount throws if AddAccountRepository fails

--- src/data/use-cases/add-account/db-add-account.spec.ts
+++ src/data/use-cases/add-account/db-add-account.spec.ts
@@ -81,4 +81,16 @@ describe('DbAddAccount UseCase', () => {
       password: 'hashed_password'
     })
   })
+  test('Should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.add(accountData)
+    expect(promise).rejects.toThrow()
+  })
 })

f98e71001fce609b8e480e0ee5bafa9755660352
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-23 16:55:59 +0100

    feat: ensure AccountMongoRepository returns account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,14 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/use-cases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const { ...accountDetails } = accountData
+    const collection = MongoHelper.getCollection('accounts')
+    const result = await collection.insertOne(accountData)
+    const account = { id: result.insertedId.toString(), ...accountDetails }
+    return account
+  }
+}

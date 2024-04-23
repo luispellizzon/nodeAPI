4113761af440eff5d31705f97c02bf3836067196
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-23 16:56:20 +0100

    test: ensure AccountMongoRepository returns account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,35 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+type SutTypes = {
+    sut: AccountMongoRepository
+}
+const makeSut = (): SutTypes => {
+  const sut = new AccountMongoRepository()
+  return {
+    sut
+  }
+}
+
+describe('insert', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('valid_name')
+    expect(account.email).toBe('valid_email')
+    expect(account.password).toBe('valid_password')
+  })
+})

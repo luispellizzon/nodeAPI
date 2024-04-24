b7b07bf6078fa909d5f8a6f44700d5e3a5cedf21
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-24 21:21:14 +0100

    feat: ensure LoginController returns 401 if invalid credentials are provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,6 +1,6 @@
 import { Authentication } from '../../../domain/use-cases/authentication'
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, serverError } from '../../helpers/http-helper'
+import { badRequest, serverError, unauthorized } from '../../helpers/http-helper'
 import { Controller, HttpResponse, HttpsRequest } from '../../protocols'
 import { RequiredFields } from '../../types/required-fields'
 import { EmailValidator } from '../signup/signup-protocols'
@@ -28,7 +28,11 @@ export class LoginController implements Controller {
       if (!this.emailValidator.isValid(email)) {
         return badRequest(new InvalidParamError(RequiredFields.Email))
       }
-      await this.authentication.auth(email, password)
+
+      const token = await this.authentication.auth(email, password)
+      if (!token) {
+        return unauthorized()
+      }
       return new Promise(resolve => resolve(null))
     } catch (error) {
       return serverError(error)
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,10 +1,16 @@
 import { AccountModel } from '../controllers/signup/signup-protocols'
 import { ServerError } from '../errors/server-error'
+import { UnauthorizedError } from '../errors/unauthorized'
 import { HttpResponse } from '../protocols/http'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
+})
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError()
 })
 
 export const serverError = (error:Error): HttpResponse => ({

40820e8885beaba6b02d7318cdf48ddec1d6742d
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-24 19:07:08 +0100

    test: ensure LoginController returns 400 if password is not provided for login

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,29 +2,40 @@ import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
 import { HttpsRequest } from '../../protocols'
 import { LoginController } from './login'
+type SutTypes = {
+    sut: LoginController,
+    emailValidator: any
+}
+const makeSut = (): SutTypes => {
+  const emailValidator = null
+  const sut = new LoginController()
+  return {
+    sut,
+    emailValidator
+  }
+}
+
+const makeFakeLoginAccount = () => ({
+  body: {
+    email: 'any_email@hotmail.com',
+    password: 'any_password'
+  }
+})
 
 describe('Login Controller', () => {
-    type SutTypes = {
-        sut: LoginController,
-        emailValidator: any
-  }
-    const makeSut = (): SutTypes => {
-      const emailValidator = null
-      const sut = new LoginController()
-      return {
-        sut,
-        emailValidator
-      }
-    }
+  test('Should return 400 if no email is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest: HttpsRequest = makeFakeLoginAccount()
+    delete httpRequest.body.email
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
 
-    test('Should return 400 if no email is provided', async () => {
-      const { sut } = makeSut()
-      const httpRequest: HttpsRequest = {
-        body: {
-          password: 'any_password'
-        }
-      }
-      const httpResponse = await sut.handle(httpRequest)
-      expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
-    })
+  test('Should return 400 if no password is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest: HttpsRequest = makeFakeLoginAccount()
+    delete httpRequest.body.password
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
 })

1eed3ee74639f5554dc1b5e880cfbe5f2eb9f6a8
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-24 12:56:14 +0100

    feat: ensure mongodb reconnects if connection is down while getting collections

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -6,7 +6,7 @@ import { MongoHelper } from '../helpers/mongo-helper'
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const { ...accountDetails } = accountData
-    const collection = MongoHelper.getCollection('accounts')
+    const collection = await MongoHelper.getCollection('accounts')
     const result = await collection.insertOne(accountData)
     const account = { id: result.insertedId.toString(), ...accountDetails }
     return account
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,9 +1,11 @@
 import { MongoClient, Collection } from 'mongodb'
 
 export class MongoHelper {
-  static client: MongoClient | null = null
+  static client: MongoClient = null
+  static uri: string = null
 
   static async connect (uri: string): Promise<void> {
+    this.uri = uri
     this.client = await MongoClient.connect(uri)
   }
 
@@ -11,9 +13,13 @@ export class MongoHelper {
     if (this.client) {
       await this.client.close()
     }
+    this.client = null
   }
 
-  static getCollection (name: string): Collection {
+  static async getCollection (name: string): Promise<Collection> {
+    if (!this.client?.db()) {
+      await this.connect(this.uri)
+    }
     return this.client.db().collection(name)
   }
 }

efd23f1ff8431165d2e041c7067663f6bef4b124
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-25 18:54:14 +0100

    test: ensure ValidationComposite returns the field error encountered

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -34,10 +34,11 @@ describe('Validation Composite', () => {
     expect(error).toEqual(new MissingParamError('field'))
   })
 
-  test('Should return an error if any validation fails', () => {
+  test('Should return the first error if more than one validation fails', () => {
     const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error())
     jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('field'))
     const error = sut.validate({ field: 'any_field' })
-    expect(error).toEqual(new MissingParamError('field'))
+    expect(error).toEqual(new Error())
   })
 })

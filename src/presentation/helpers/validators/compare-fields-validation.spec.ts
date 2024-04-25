9583b93156779799ea44b14b64b32db18bd3b5ef
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-25 18:35:50 +0100

    refactor: change tests to be of CompareFieldsValidation

--- src/presentation/helpers/validators/compare-fields-validation.spec.ts
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -2,24 +2,22 @@ import { InvalidParamError } from '../../errors/invalid-param-error'
 import { CompareFieldsValidation } from './compare-fields-validation'
 
 const makeSut = (): CompareFieldsValidation => {
-  return new CompareFieldsValidation('password', 'confirmationPassword')
+  return new CompareFieldsValidation('field', 'fieldToCompare')
 }
 
-describe('Password Confirmation Validation', () => {
-  test('Should call PasswordConfirmationValidation with correct values', () => {
+describe('Compare Fields Validation', () => {
+  test('Should call CompareFieldsValidation with correct values', () => {
     const sut = makeSut()
     const validateSpy = jest.spyOn(sut, 'validate')
-
-    sut.validate({ password: 'any_password', confirmationPassword: 'any_password' })
-
+    sut.validate({ field: 'any_field', fieldToCompare: 'any_field' })
     expect(validateSpy)
-      .toHaveBeenCalledWith({ password: 'any_password', confirmationPassword: 'any_password' })
+      .toHaveBeenCalledWith({ field: 'any_field', fieldToCompare: 'any_field' })
   })
 
   // Test confirmation password fails if not equal to password
-  test('Should return 400 if password is not equal to confirmation password', async () => {
+  test('Should return InvalidParamError if fields are not equal', async () => {
     const sut = makeSut()
-    const error = sut.validate({ password: 'any_password', confirmationPassword: 'different_password' })
-    expect(error).toEqual(new InvalidParamError('confirmationPassword'))
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'different_value' })
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
   })
 })

045fdd7fb4a681994e92fe4f24fc7c7b7a5bcbc7
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-25 18:49:09 +0100

    refactor: create sut factory to simulate different error results

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -3,23 +3,34 @@ import { RequiredFields } from '../../types/required-fields'
 import { Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
 
-const makeSut = () => {
+const makeValidation = () => {
   class ValidationStub implements Validation {
     validate (input: any): Error {
-      return new MissingParamError('field')
+      return null
     }
   }
-  return new ValidationComposite([new ValidationStub()])
+  return new ValidationStub()
+}
+
+type SutTypes = {
+    sut: ValidationComposite,
+    validationStub: Validation
+}
+const makeSut = ():SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite([validationStub])
+
+  return {
+    sut,
+    validationStub
+  }
 }
 
 describe('Validation Composite', () => {
   test('Should return an error if any validation fails', () => {
-    const sut = makeSut()
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
     const error = sut.validate({ field: 'any_field' })
     expect(error).toEqual(new MissingParamError('field'))
-  })
-
-  test('Should return falsy if validation succeeds', () => {
-
   })
 })

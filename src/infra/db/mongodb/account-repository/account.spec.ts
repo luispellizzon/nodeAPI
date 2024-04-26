feb3ff841786b2a6f917ee0f04448370b0e8c1f0
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-26 16:13:13 +0100

    test: ensure AccountMongoRepository returns account on load by Email success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,3 +1,4 @@
+import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
 
@@ -10,8 +11,8 @@ const makeSut = (): SutTypes => {
     sut
   }
 }
-
-describe('insert', () => {
+let collection: Collection
+describe('Account Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -19,10 +20,10 @@ describe('insert', () => {
     await MongoHelper.disconnect()
   })
   beforeEach(async () => {
-    const collection = await MongoHelper.getCollection('accounts')
+    collection = await MongoHelper.getCollection('accounts')
     await collection.deleteMany({})
   })
-  test('Should return an account on success', async () => {
+  test('Should return an account on add success', async () => {
     const { sut } = makeSut()
     const accountData = {
       name: 'valid_name',
@@ -34,6 +35,22 @@ describe('insert', () => {
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('valid_name')
     expect(account.email).toBe('valid_email')
+    expect(account.password).toBe('valid_password')
+  })
+
+  test('Should return an account on loadByEmail success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@hotmail.com',
+      password: 'valid_password'
+    }
+    await collection.insertOne(accountData)
+    const account = await sut.loadByEmail('valid_email@hotmail.com')
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('valid_name')
+    expect(account.email).toBe('valid_email@hotmail.com')
     expect(account.password).toBe('valid_password')
   })
 })

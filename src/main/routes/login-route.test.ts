f58d0e8e7fc052415be0a09d2f37759fd8bcc0cb
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-26 18:22:12 +0100

    test: returns 401 unauthorized on login

--- src/main/routes/login-route.test.ts
+++ src/main/routes/login-route.test.ts
@@ -47,5 +47,15 @@ describe('Login Route', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'valid_email@gmail.com',
+          password: 'valid_password'
+        })
+        .expect(401)
+    })
   })
 })

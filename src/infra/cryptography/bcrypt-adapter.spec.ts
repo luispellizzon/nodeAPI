fc747ed5ac1a8a1d30788da5a716be07e5bdad26
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-26 12:50:13 +0100

    test: ensure BcryptAdapter throws error if bcrypt compare throws

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -68,4 +68,13 @@ describe('Bcrypt Adapter', () => {
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(false)
   })
+
+  test('Should throw if bcrypt compare throws', async () => {
+    const { sut } = makeSut()
+    jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.compare('any_value', 'any_hash')
+    expect(promise).rejects.toThrow()
+  })
 })

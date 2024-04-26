9cd391f94363e9b2b4db51128d94e5cdd4c35501
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-26 18:20:37 +0100

    test: implements login route

--- src/main/routes/login-route.ts
+++ src/main/routes/login-route.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { makeSignUpController } from '../factories/signup/signup-factory'
+import { makeLoginController } from '../factories/login/login-factory'
 import { expressRouteAdapter } from '../adapters/express/express-route-adapter'
 
 export default (router: Router): void => {
   router.post('/signup', expressRouteAdapter(makeSignUpController()))
+  router.post('/login', expressRouteAdapter(makeLoginController()))
 }

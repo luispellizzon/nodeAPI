109f1d8a6f9fd0583272f86ac6db86786dace905
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-04-26 15:16:59 +0100

    refactor: move bcrupt adapter to its own folder

--- src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -1,5 +1,5 @@
-import { HashComparer } from '../../data/protocols/cryptography/hash-comparer'
-import { Hasher } from '../../data/protocols/cryptography/hasher'
+import { HashComparer } from '../../../data/protocols/cryptography/hash-comparer'
+import { Hasher } from '../../../data/protocols/cryptography/hasher'
 import bcrypt from 'bcrypt'
 export class BcryptAdapter implements Hasher, HashComparer {
   private readonly salt: number

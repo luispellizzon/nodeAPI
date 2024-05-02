d92561e09d875936c3f50488ddca805baa9d1f2d
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-02 17:42:58 +0100

    feat: ensure AuthMiddleware returns 200 if LoadAccountByToken succeeds

--- src/presentation/middlewares/auth/auth-middleware.ts
+++ src/presentation/middlewares/auth/auth-middleware.ts
@@ -1,6 +1,6 @@
 import { LoadAccountByToken } from '../../../domain/use-cases/load-account-by-token'
 import { AccessDeniedError } from '../../errors/access-denied'
-import { forbidden } from '../../helpers/http/http-helper'
+import { forbidden, success } from '../../helpers/http/http-helper'
 import { HttpResponse, HttpsRequest, Middleware } from '../../protocols'
 
 export class AuthMiddleware implements Middleware {
@@ -12,6 +12,9 @@ export class AuthMiddleware implements Middleware {
     const accessToken = httpRequest.headers?.['x-access-token']
     if (accessToken) {
       const user = await this.loadAccountByToken.load(accessToken)
+      if (user) {
+        return success({ accountId: user.id })
+      }
     }
     return forbidden(new AccessDeniedError())
   }

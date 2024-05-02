91599013b7dbf5407626dee7e0df3f7f8aa2362e
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-02 22:54:05 +0100

    test: ensure add survey returns 204 if accessToken is provided and user is admin

--- src/main/routes/survey-route.test.ts
+++ src/main/routes/survey-route.test.ts
@@ -2,8 +2,11 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
-let collection:Collection
+let collection: Collection
+let accountCollection: Collection
 describe('Survey Route', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -12,8 +15,10 @@ describe('Survey Route', () => {
     await MongoHelper.disconnect()
   })
   beforeEach(async () => {
-    collection = await MongoHelper.getCollection('survey')
+    collection = await MongoHelper.getCollection('surveys')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await collection.deleteMany({})
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
@@ -31,6 +36,38 @@ describe('Survey Route', () => {
           }]
         })
         .expect(403)
+    })
+
+    test('Should return 204 on add survey with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'valid_name',
+        email: 'valid_email@gmail.com',
+        password: 'valid_password',
+        role: 'admin'
+      })
+      const accessToken = sign({ id: res.insertedId.toString() }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: res.insertedId
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      console.log(accessToken)
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'any_question',
+          answers: [{
+            icon: 'any_image',
+            answer: 'any_answer'
+          },
+          {
+            answer: 'any_answer2'
+          }]
+        })
+        .expect(204)
     })
   })
 })

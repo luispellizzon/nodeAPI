d232326572ba3f4d536eaea767bbb76e94249898
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-03 17:01:53 +0100

    feat: ensure LoadSurveysController calls LoadSurveys protocol

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,12 @@
+import { Controller, HttpResponse, HttpsRequest, LoadSurveys } from './load-surveys-controller-protocols'
+
+export class LoadSurveysController implements Controller {
+  constructor (private readonly loadSurveys: LoadSurveys) {
+    this.loadSurveys = loadSurveys
+  }
+
+  async handle (httpsRequest: HttpsRequest): Promise<HttpResponse> {
+    await this.loadSurveys.load()
+    return new Promise(resolve => resolve(null))
+  }
+}

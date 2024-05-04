a545aebec6cb4b23e008ec7d46242533967a3151
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-04 13:37:15 +0100

    feat: ensure SurveyMongoRepository load all surveys on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,8 +1,17 @@
+import { LoadSurveysRepository } from '../../../../data/protocols/db/survey/load-survey-repository'
 import { AddSurveyModel, AddSurveyRepository } from '../../../../data/use-cases/add-survey/add-survey-protocols'
+import { SurveyModel } from '../../../../domain/models/survey-model'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (accountData: AddSurveyModel): Promise<void> {
     (await MongoHelper.getCollection('surveys')).insertOne(accountData)
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const collection = await MongoHelper.getCollection('surveys')
+    const surveys = await collection.find().toArray()
+    const mappedSurveys = surveys.map(survey => MongoHelper.map<SurveyModel>(survey))
+    return mappedSurveys
   }
 }

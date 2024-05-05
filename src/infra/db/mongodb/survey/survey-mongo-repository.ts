20e4213dfa412ac0ae53ebde4516dd3e80df7e82
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-05 21:41:27 +0100

    feat: ensure SurveyMongoRepository returns null if survey is not found by its id

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,7 +1,8 @@
-import { LoadSurveysRepository, AddSurveyModel, AddSurveyRepository, SurveyModel } from './survey-mongo-repository-protocols'
+import { LoadSurveysRepository, LoadSurveyByIdRepository, AddSurveyModel, AddSurveyRepository, SurveyModel } from './survey-mongo-repository-protocols'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { ObjectId } from 'mongodb'
 
-export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
   async add (accountData: AddSurveyModel): Promise<void> {
     (await MongoHelper.getCollection('surveys')).insertOne(accountData)
   }
@@ -11,5 +12,11 @@ export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRe
     const surveys = await collection.find().toArray()
     const mappedSurveys = surveys.map(survey => MongoHelper.map<SurveyModel>(survey))
     return mappedSurveys
+  }
+
+  async loadById (surveyId: string): Promise<SurveyModel> {
+    const collection = await MongoHelper.getCollection('surveys')
+    const survey = await collection.findOne({ _id: new ObjectId(surveyId) })
+    return survey ? MongoHelper.map<SurveyModel>(survey) : null
   }
 }

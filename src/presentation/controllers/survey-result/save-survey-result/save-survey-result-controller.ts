c9bc86784cea0f4ab48e2c9de7588349b668fb0b
Luis Pellizzon <luis.pellizzon@gmail.com>
2024-05-06 14:16:24 +0100

    feat: ensure SaveSurveyResultController returns serverError if LoadById throws

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,4 +1,4 @@
-import { Controller, HttpResponse, HttpsRequest, LoadSurveyById, forbidden, AccessDeniedError, InvalidParamError } from './save-survey-result-controller-protocols'
+import { Controller, HttpResponse, HttpsRequest, LoadSurveyById, forbidden, AccessDeniedError, InvalidParamError, serverError } from './save-survey-result-controller-protocols'
 
 export class SaveSurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) {
@@ -6,10 +6,14 @@ export class SaveSurveyResultController implements Controller {
   }
 
   async handle (httpRequest: HttpsRequest): Promise<HttpResponse> {
-    const { surveyId } = httpRequest.params
-    const isSurvey = await this.loadSurveyById.loadById(surveyId)
-    if (!isSurvey) {
-      return forbidden(new InvalidParamError('survey_id'))
+    try {
+      const { surveyId } = httpRequest.params
+      const isSurvey = await this.loadSurveyById.loadById(surveyId)
+      if (!isSurvey) {
+        return forbidden(new InvalidParamError('survey_id'))
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
